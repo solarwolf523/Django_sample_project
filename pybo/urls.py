2a59141d24e19eb4666e77d8a10565046511c964
codenavy94 <codenavy94@yonsei.ac.kr>
2021-10-14 18:01:51 +0900

    models, Question and Answer, created

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index) # 웹에서 url 127.0.0.1:8000/pybo를 치면 config/urls.py에서 pybo/와 매칭 -> pybo/urls.py에서 pybo/views.index와 매칭, 결과값 리턴
 ]
